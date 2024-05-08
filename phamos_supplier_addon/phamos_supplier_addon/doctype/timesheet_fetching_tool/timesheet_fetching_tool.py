91b4c3cec60d0d7dc7c8214aa426f735e636b5e4
beingeek <furqan.790@outlook.com>
2024-05-08 13:59:08 +0000

    feat: added employee field in timesheet

--- phamos_supplier_addon/phamos_supplier_addon/doctype/timesheet_fetching_tool/timesheet_fetching_tool.py
+++ phamos_supplier_addon/phamos_supplier_addon/doctype/timesheet_fetching_tool/timesheet_fetching_tool.py
@@ -23,11 +23,12 @@ class TimesheetFetchingTool(Document):
 			timesheet_data = response.json().get("data")
 
 			phamos_project = timesheet_data.get("project")
-			project_id = get_project_id(phamos_project, company, self)
+			project_id = get_project_id(phamos_project, company, self) if phamos_project else ""			
 
 			if not frappe.db.exists("Timesheet", {"custom_phamos_timesheet": phamos_timesheet, "custom_phamos_timesheet_record": phamos_timesheet_record}):
 				new_timesheet = frappe.new_doc("Timesheet")
 				new_timesheet.update(timesheet_data)
+				new_timesheet.employee = get_employee(timesheet_data.get("employee"), company, self) if timesheet_data.get("employee") else ""
 				new_timesheet.custom_phamos_timesheet_record = phamos_timesheet_record
 				new_timesheet.custom_phamos_timesheet = phamos_timesheet
 				new_timesheet.company = company
@@ -36,16 +37,15 @@ class TimesheetFetchingTool(Document):
 				new_timesheet.customer = ""
 				new_timesheet.parent_project = ""
 				new_timesheet.name = ""
-				new_timesheet.employee = ""
 				new_timesheet.docstatus = 0
 				new_timesheet.status = "Draft"
 				for log in new_timesheet.time_logs:
 					log.custom_phamos_timesheet_record = phamos_timesheet_record
 					log.custom_phamos_timesheet = phamos_timesheet
 					log.custom_phamos_project = phamos_project
-					log.project = get_project_id(log.project, company, self)
+					log.project = get_project_id(log.project, company, self) if log.project else ""
 					log.task = ""
-					log.activity_type = ""
+					log.activity_type = get_activity_type(log.get("activity_type")) if log.activity_type else ""
 					log.sales_invoice = ""
 				
 				new_timesheet.insert()
@@ -90,13 +90,48 @@ def get_project_id(phamos_project, company, timesheet_fetching_tool_doc):
 		new_project = frappe.new_doc("Project")
 		new_project.update(project_data)
 		new_project.custom_phamos_project = phamos_project
-		new_project.name = ""
+		new_project.expected_start_date = ""
+		new_project.expected_end_date = ""
 		new_project.cost_center = ""
-		new_project.department = ""
 		new_project.sales_order = ""
+		new_project.department = ""
 		new_project.customer = ""
+		new_project.name = ""
 		new_project.company = company
 		new_project.insert()
 		project_id = new_project.name
 	
 	return project_id
+
+
+def get_employee(phamos_employee, company, timesheet_fetching_tool_doc):
+	employee_id = frappe.db.exists("Employee", {"custom_phamos_employee": phamos_employee})
+	if not employee_id:
+		url = f"{timesheet_fetching_tool_doc.url}/api/resource/Employee/{phamos_employee}"
+		response = requests.request("GET", url, headers=timesheet_fetching_tool_doc.cookie_headers)
+		employee_data = response.json().get("data")
+
+		new_employee = frappe.new_doc("Employee")
+		new_employee.update(employee_data)
+		new_employee.custom_activity_type = get_activity_type(employee_data.get("activity_type")) if employee_data.get("activity_type") else ""
+		new_employee.custom_phamos_employee = phamos_employee
+		new_employee.company = company
+		new_employee.leave_approver = ""
+		new_employee.holiday_list = ""
+		new_employee.reports_to = ""
+		new_employee.department = ""
+		new_employee.user_id = ""
+		new_employee.name = ""
+		new_employee.insert()
+		employee_id = new_employee.name
+	
+	return employee_id
+
+
+def get_activity_type(activity_type):
+	if not frappe.db.exists("Activity Type", activity_type):
+		new_activity = frappe.new_doc("Activity Type")
+		new_activity.activity_type = activity_type
+		new_activity.insert()
+	
+	return activity_type
